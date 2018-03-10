0d3c4a4cc52b9b1581ac7a5d0a56618fa1055307
pangsen <pangs@shinetechchina.com>
2018-03-10 20:18:27 +0800

    start easy worker

---
+++ Worker.Test/WorkerTest.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker.Test
+{
+
+    public class StringMessage : IMessgae
+    {
+        public string Message { get; set; }
+    }
+
+    public class IntMessage : IMessgae
+    {
+        public int Message { get; set; }
+    }
+
+    public class StringMessageHandler : IHander<StringMessage>
+    {
+        public void Handle(StringMessage message)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(message));
+        }
+    }
+    public class SecondStringMessageHandler : IHander<StringMessage>
+    {
+        public void Handle(StringMessage message)
+        {
+            Console.WriteLine("SecondStringMessageHandler:" + JsonConvert.SerializeObject(message));
+        }
+    }
+    public class IntMessageHandler : IHander<IntMessage>
+    {
+
+        public void Handle(IntMessage message)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(message));
+        }
+
+    }
+    public class WorkerTest
+    {
+        [Test]
+        public void Test()
+        {
+            var worker = new Worker();
+
+            worker.AddHandler(new IntMessageHandler());
+            worker.AddHandler(new StringMessageHandler());
+            worker.AddHandler(new SecondStringMessageHandler());
+
+            worker.AddJob(new StringMessage() { Message = "strting Message" });
+            worker.AddJob(new IntMessage() { Message = 100 });
+
+            Task.Run(() => worker.Start());
+            while (worker.HasValue())
+            {
+                Task.Delay(TimeSpan.FromMilliseconds(10)).GetAwaiter().GetResult();
+            }
+            worker.Stop();
+        }
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    
+    public interface IMessgae { }
+    public interface IHander<in T> where T : IMessgae
+    {
+        void Handle(T message);
+    }
+    public class Worker
+    {
+        private object _lock = new object();
+        private Queue<IMessgae> _queue { get; set; }
+        private List<IMessgae> _pending { get; set; }
+        private List<object> _handlers { get; set; }
+        private int _maxTaskCount { get; set; } = 10;
+        private bool _stop { get; set; }
+        private TimeSpan _delayTimeSpan { get; set; } = TimeSpan.FromSeconds(5);
+
+        public Worker()
+        {
+            _queue = new Queue<IMessgae>();
+            _pending = new List<IMessgae>();
+            _handlers = new List<object>();
+        }
+        public void AddJob<T>(T t) where T : IMessgae
+        {
+            lock (_lock)
+            {
+                _queue.Enqueue(t);
+            }
+
+        }
+        public void AddJobs<T>(IEnumerable<T> list) where T : IMessgae
+        {
+            lock (_lock)
+            {
+                foreach (var item in list)
+                {
+                    AddJob(item);
+                }
+            }
+        }
+        public void AddHandler<T>(IHander<T> handler) where T : IMessgae
+        {
+            _handlers.Add(handler);
+        }
+        public void Start()
+        {
+
+            while (!_stop)
+            {
+                if (_pending.Count < _maxTaskCount && _queue.Any())
+                {
+                    IMessgae message;
+                    lock (_lock)
+                    {
+                        message = _queue.Dequeue();
+                    }
+
+                    _pending.Add(message);
+
+                    Task.Run(() =>
+                    {
+                        Handle((dynamic)message);
+                        _pending.Remove(message);
+                    });
+                }
+                else if (!_queue.Any())
+                {
+                    Task.Delay(_delayTimeSpan).GetAwaiter().GetResult();
+                }
+                else
+                {
+                    Task.Delay(TimeSpan.FromMilliseconds(10)).GetAwaiter().GetResult();
+                }
+            }
+            while (_pending.Any())
+            {
+                Task.Delay(TimeSpan.FromMilliseconds(100)).GetAwaiter().GetResult();
+            }
+        }
+        public void Stop()
+        {
+            _stop = true;
+        }
+        public void SetDelayTime(TimeSpan delayTime)
+        {
+            _delayTimeSpan = delayTime;
+        }
+        public bool HasValue()
+        {
+            return _queue.Any();
+        }
+        private void Handle<T>(T message) where T : IMessgae
+        {
+            _handlers.ForEach(a =>
+            {
+                if (typeof(IHander<>).MakeGenericType(message.GetType()).IsAssignableFrom(a.GetType()))
+                {
+                    var handler = (IHander<T>)a;
+                    handler.Handle(message);
+                }
+            });
+        }
+    }
+}
